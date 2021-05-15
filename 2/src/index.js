a2a2f1a1ef37bcb9ab8b291aac77f5aa56102c7d
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-05-15 18:30:01 -0300

    e

---
+++ 2/src/index.js
@@ -0,0 +1,12 @@
+console.clear()
+console.log('hola')
+var a = 0;
+var b = 5;
+if (a == 0) {
+    if (b > 0) {
+        console.log('ganaste bro');
+    }
+    else{
+        console.log('Perdiste');
+    }
+}
