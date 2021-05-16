c2254fc6d72c98ab6cfcf2581150c1dabcb6fe30
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-05-16 16:58:45 -0300

    :D

--- 3/src/index.js
+++ 3/src/index.js
@@ -1,12 +1,29 @@
-console.clear()
-console.log('hola')
-var a = 0;
-var b = 5;
-if (a == 0) {
-    if (b > 0) {
-        console.log('ganaste bro');
-    }
-    else{
-        console.log('Perdiste');
-    }
+var numero_ranom = function (pow) {
+	return Math.floor(Math.random() * pow);
+};
+var repartidores = numero_ranom (100);
+const feriado = Boolean(Math.round(Math.random()));
+if (feriado){
+    porcentaje_sueldos = 80;
 }
+else{
+    porcentaje_sueldos = 65;
+}
+var gananciasDia = numero_ranom (1000);
+
+var porcentaje_sueldos;
+
+var Repartir_Sueldos = (gananciasDia/100) * porcentaje_sueldos;
+
+var Sueldo_Repartidos = Repartir_Sueldos / repartidores;
+
+var gananciasFinales = gananciasDia-Repartir_Sueldos;
+
+
+console.log("Repartidores:",repartidores);
+console.log("Feriado:",feriado);
+console.log("Ganancias_Dia:",gananciasDia);
+console.log("Porcentaje_Sueldos:",porcentaje_sueldos);
+console.log("Repartir a Sueldos:",Repartir_Sueldos);
+console.log("Sueldo_Repartidos:",Sueldo_Repartidos);
+console.log("Ganancias_Finales:", gananciasFinales);
