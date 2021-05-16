84b6f59b9b04f6ac7bad7c7b359597d807a4b8e0
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-05-16 16:07:23 -0300

    number

--- 2/src/index.js
+++ 2/src/index.js
@@ -1,12 +1,19 @@
-console.clear()
-console.log('hola')
-var a = 0;
-var b = 5;
-if (a == 0) {
-    if (b > 0) {
-        console.log('ganaste bro');
-    }
-    else{
-        console.log('Perdiste');
-    }
+
+var numero_ranom = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1 ) + min);
+};
+var rango = numero_ranom (1500,3000);
+console.log("Numero: ",rango);
+
+var porcentaje = numero_ranom (25,100);
+console.log("Porcentaje: ",porcentaje);
+
+var aumento = ((rango/100) * porcentaje) ;
+console.log("Aumento: ", aumento);
+
+var precio_final = aumento + rango;
+console.log("Precio final: ",precio_final);
+
+if (precio_final>4000) {
+    console.log("Precio excedido");
 }
