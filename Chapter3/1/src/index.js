9ef6a25b7ad5e4d16250cafcbdae4f8ad5e13bce
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-05-21 10:26:29 -0300

    chapter3

---
+++ Chapter3/1/src/index.js
@@ -0,0 +1,7 @@
+console.clear()
+var uno = "Hola";
+function imprimir(){
+    var dos = "Hola bobo";
+    console.log(dos);
+}
+imprimir();
---
+++ Chapter3/2/src/index.js
@@ -0,0 +1,19 @@
+/*
+Emanuel quiere cocinar galletitas y quiere un programa que lo ayude a saber que debe
+comprar y en caso que cuente con todos los ingredientes, informar que puede cocinar las
+galletas. La receta pide los siguientes ingredientes
+3 huevos
+1 barra de chocolate
+0.5 kilos de harina
+5 cucharadas de azucar
+Los ingredientes que tiene Ema tienen que ser variables al inicio del programa que sean
+faciles de editar
+*/
+console.clear()
+var huevos = 3;
+var chocolate = 1;
+var harina = 0.5;
+var azucar_cucharadas = 5;
+if ((huevos == 3) && ( chocolate == 1) && (harina == 0.5) && (azucar_cucharadas == 5)) {
+    console.count('Tenes todos los ingredientes! ');
+}
